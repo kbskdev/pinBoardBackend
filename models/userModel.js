3fe11d5dfc69e616d473adf9a3d109cd6670c0db
kbskdev <milosz.olsztyn@gmail.com>
2023-03-09 16:50:38 +0100

    email model

--- models/userModel.js
+++ models/userModel.js
@@ -63,9 +63,9 @@ const UserSchema = new mongoose.Schema({
     },
     email:{
         type:String,
-        unique:[true,"that email is already used"],
-        //required:[true,"email must be included"],
-        validate:[validator.isEmail,'invalid email']
+        // unique:[true,"that email is already used"],
+        // //required:[true,"email must be included"],
+        // validate:[validator.isEmail,'invalid email']
     },
     password:{
         type:String,
