5fd0467660df736940804e144fa20a25a28ba203
kbskdev <milosz.olsztyn@gmail.com>
2023-03-12 19:53:27 +0100

    final changes to logs controllers i hope

--- controllers/requestLogger.js
+++ controllers/requestLogger.js
@@ -2,9 +2,9 @@ const fs = require('fs')
 
 module.exports = async(req,res,next)=>{
 
-    fs.appendFile('./logs/requestLog.txt',` \n\n request:${req.url}
-                                                         requestBody:${req.body}
-                                                         on:${Date()}`,
+    fs.appendFile('./logs/requestLog.txt',`\n\n request:${req.url}
+    requestBody:${req.body}
+    on:${Date()}`,
         'utf-8',(err)=>{})
     next()
 }
