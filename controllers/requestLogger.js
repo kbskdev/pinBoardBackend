e7440d3fcf6512b9537451555332b5dd71fe2ae8
kbskdev <milosz.olsztyn@gmail.com>
2023-03-12 19:51:51 +0100

    requests logs

--- controllers/requestLogger.js
+++ controllers/requestLogger.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 module.exports = async(req,res,next)=>{
 
     fs.appendFile('./logs/requestLog.txt',` \n\n request:${req.url}
-                                                        \n requestBody:${req.body}
-                                                        \n on:${Date()}`,
+                                                         requestBody:${req.body}
+                                                         on:${Date()}`,
         'utf-8',(err)=>{})
     next()
 }
