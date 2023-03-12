4683fb44812cbcee3dcf7eed5aa450623c9a1b10
kbskdev <milosz.olsztyn@gmail.com>
2023-03-12 19:51:28 +0100

    requests logs

--- controllers/erroController.js
+++ controllers/erroController.js
@@ -10,8 +10,8 @@ module.exports = async(err,req,res,next)=>{
     })
 
     fs.appendFile('./logs/errorLog.txt',`\n\n request:${err.request.url}
-                                                    \n requestBody:${JSON.stringify(err.request.body)}
-                                                    \n ${err} on:${Date()}`,
+                                                     requestBody:${JSON.stringify(err.request.body)}
+                                                     ${err} on:${Date()}`,
         'utf-8',(err)=>{
         if(err) return console.log(err)
         console.log('error logged')
