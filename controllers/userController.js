9542da2e63fee0dd4249331ad01319a650b449bc
kbskdev <milosz.olsztyn@gmail.com>
2023-03-25 11:27:42 +0100

    is author test

--- controllers/userController.js
+++ controllers/userController.js
@@ -21,7 +21,7 @@ exports.addUser = async(req,res,next)=>{
 }
 exports.isAuthor = async (req,res,next)=>{
     try{
-        const isAuthor = User.findOne({'composition._id':req.body.composition})
+        const isAuthor = User.findOne({_id:req.body.author})
         res.status(200).json({
             status:'success',
             data:isAuthor
