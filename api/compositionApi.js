1098256bc3d8cec381a51f93e951417ba08970c2
kbskdev <milosz.olsztyn@gmail.com>
2023-03-26 00:53:16 +0100

    is author test

--- api/compositionApi.js
+++ api/compositionApi.js
@@ -12,7 +12,7 @@ router.delete('/deleteImage/:composition/:image',compositionController.deleteIma
 router.patch('/updateImageOrder/:composition/:image/:order',compositionController.changeImageOrder)
 router.patch('/updateImagePosition/:composition/:image/:x/:y',compositionController.changeImagePosition)
 router.get('/getCompositionList/',compositionController.getCompositionList)
-router.get('/getOneComp/:composition',compositionController.getOneComp)
+router.get('/getOneComp/:composition/',compositionController.getOneComp)
 router.get('/getImage/:composition/:image',compositionController.getImage)
 
 module.exports = router;
