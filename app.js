b0578910de5e8db96a4f2bd30260e11fe234e17a
kbskdev <milosz.olsztyn@gmail.com>
2023-03-25 22:03:42 +0100

    is author test

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json())
 app.use(cors())
 app.use('/api/v1/users/login/',requestLogger,authController.login)
 app.use('/api/v1/users/',requestLogger,require('./api/userApi'))
-app.use('api/v1/images/getOneCompPublic/:composition',(req,res,next)=>getOneCompPublic)
+app.use('/api/v1/images/getOneCompPublic/:composition',(req,res,next)=>getOneCompPublic)
 app.use('/api/v1/images/',authController.authorize,requestLogger,require('./api/compositionApi'))
 app.all('*',(req,res,next)=>{next(new ErrorHandler(req,'bad route',404))})
 
