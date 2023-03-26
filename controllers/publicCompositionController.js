9a25a146fcb06246870ecf273056baa0a0d19be3
kbskdev <milosz.olsztyn@gmail.com>
2023-03-26 19:17:34 +0200

    geting public comp images, comps

--- controllers/publicCompositionController.js
+++ controllers/publicCompositionController.js
@@ -1,5 +1,6 @@
 const ErrorHandler = require('../common/ErrorHandler')
 const UserModel = require('../models/userModel')
+const mongoose = require("mongoose");
 
 exports.getPublicCompList = async(req,res,next)=>{
     try{
@@ -15,8 +16,8 @@ exports.getPublicCompList = async(req,res,next)=>{
 
 exports.getOneCompPublic = async(req,res,next)=>{
     try{
-        const imageList = await UserModel.User.findOne({_id:req.params.user,'composition._id':req.params.composition,},{_id:0,composition:1})
-
+        const imageList = await UserModel.User.findOne({_id:req.params.user,'composition._id':mongoose.Types.ObjectId(req.params.composition)},
+            {_id:0,'composition.$':1})
         res.status(200).json({
             status:'success',
             data:imageList
@@ -32,7 +33,9 @@ exports.getImagePublic = async(req,res,next)=>{
         if(imageList!=null){
             res.sendFile(`./${req.params.user}/${req.params.composition}/${req.params.image}`,{root:'userData'})
         }
-        else throw "image is in not public board"
+        else { // noinspection ExceptionCaughtLocallyJS
+            throw "image is in not public board"
+        }
     }catch (err){
         return next(new ErrorHandler(req,err,400))
     }
