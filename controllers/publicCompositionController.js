46dec0d3fb5598a54ce1c666e41f9dad83e189ab
kbskdev <milosz.olsztyn@gmail.com>
2023-03-26 19:07:53 +0200

    geting public comp images, comps

--- controllers/publicCompositionController.js
+++ controllers/publicCompositionController.js
@@ -15,10 +15,11 @@ exports.getPublicCompList = async(req,res,next)=>{
 
 exports.getOneCompPublic = async(req,res,next)=>{
     try{
-        const imageList = await UserModel.User.findOne({_id:req.params.user,'composition._id':req.params.composition,},{_id:0,'composition.$':1})
+        const imageList = await UserModel.User.findOne({_id:req.params.user,'composition._id':req.params.composition,},{_id:0,'composition.$':1}).findOne({public:"public"})
+
         res.status(200).json({
             status:'success',
-            data:imageList.public
+            data:imageList
         })
     }catch (err){
         return next(new ErrorHandler(req,err,400))
