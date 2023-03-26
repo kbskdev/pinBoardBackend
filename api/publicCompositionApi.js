3e58cd5e05c52f5ef32d535c3d51399119caa27a
kbskdev <milosz.olsztyn@gmail.com>
2023-03-26 16:56:31 +0200

    geting public comp images, comps

--- api/publicCompositionApi.js
+++ api/publicCompositionApi.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const publicCompositionController = require('../controllers/publicCompositionController')
 
 router.get('/getCompositionList/:user',publicCompositionController.getPublicCompList)
-router.get('/getOneComp/:composition/',publicCompositionController.getOneCompPublic)
+router.get('/getOneComp/:user/:composition/',publicCompositionController.getOneCompPublic)
 router.get('/getImage/:user/:composition/:image',publicCompositionController.getImagePublic)
 
 module.exports = router;
