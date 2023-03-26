8cb8987d49e5e0eb318723dfd5345d535b6f99af
kbskdev <milosz.olsztyn@gmail.com>
2023-03-26 21:43:44 +0200

    geting public comp images, comps

--- controllers/publicCompositionController.js
+++ controllers/publicCompositionController.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 
 exports.getPublicCompList = async(req,res,next)=>{
     try{
-        const compositionList = await UserModel.User.aggregate([{$match:{_id:mongoose.Types.ObjectId(req.params.user)},'composition.public':'public'},{$project:{composition:1}}])
+        const compositionList = await UserModel.User.aggregate([{$match:{_id:mongoose.Types.ObjectId(req.params.user),'composition.public':'public'}},{$project:{composition:1}}])
         res.status(200).json({
             status:'success',
             data:compositionList
