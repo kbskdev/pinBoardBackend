9b1aea174c0a6f6c4f5691ad97ba10f6e95145d3
kbskdev <milosz.olsztyn@gmail.com>
2023-06-27 17:44:07 +0200

    imageInfo update

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const requestLogger = require("./controllers/requestLogger")
 app = express()
 app.use(express.json())
 //app.use(bodyParser.urlencoded({ extended: true }))
-app.use(
-    cors({origin: ['http://localhost:8888', 'http://127.0.0.1:8888']})
-);
+app.use(cors({origin: '*'}));
 app.use('/api/v1/users/login/',requestLogger,authController.login)
 app.use('/api/v1/users/',requestLogger,require('./api/userApi'))
 app.use('/api/v1/publicImages/',requestLogger,require('./api/publicCompositionApi'))
