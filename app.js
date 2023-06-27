5ecc36b68d7941066926e1f7d0ef2c16b220c758
kbskdev <milosz.olsztyn@gmail.com>
2023-06-27 17:51:04 +0200

    making cors work properly

--- app.js
+++ app.js
@@ -8,7 +8,24 @@ const requestLogger = require("./controllers/requestLogger")
 app = express()
 app.use(express.json())
 //app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors({origin: '*'}));
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 app.use('/api/v1/users/login/',requestLogger,authController.login)
 app.use('/api/v1/users/',requestLogger,require('./api/userApi'))
