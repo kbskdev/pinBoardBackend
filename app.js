389958a5734d34c7c592680aa5c78b49fd502d60
kbskdev <milosz.olsztyn@gmail.com>
2023-06-27 17:38:11 +0200

    imageInfo update

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const ErrorHandler = require('./common/ErrorHandler')
 const authController = require("./controllers/authController");
 const requestLogger = require("./controllers/requestLogger")
 
-const compositionController = require("./controllers/compositionController")
-
 app = express()
 app.use(express.json())
 //app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
+app.use(
+    cors({origin: ['http://localhost:8888', 'http://127.0.0.1:8888']})
+);
 app.use('/api/v1/users/login/',requestLogger,authController.login)
 app.use('/api/v1/users/',requestLogger,require('./api/userApi'))
 app.use('/api/v1/publicImages/',requestLogger,require('./api/publicCompositionApi'))
