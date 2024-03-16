46387128bca2aca3afa9ca7c9413e6f1acdcc0e2
kbskdev <milosz.olsztyn@gmail.com>
2024-03-16 06:12:54 +0100

    demo

--- controllers/publicCompositionController.js
+++ controllers/publicCompositionController.js
@@ -41,11 +41,12 @@ exports.getPublicCompListByUser = async(req,res,next)=>{
 
 exports.getOneCompPublic = async(req,res,next)=>{
     try{
-        const imageList = await UserModel.User.findOne({_id:req.params.user},)
+        const imageList = await UserModel.User.findOne({_id:req.params.user,'composition._id':mongoose.Types.ObjectId(req.params.composition)},{_id:0,'composition.$':1})
 
         res.status(200).json({
             status:'success',
-            data:imageList
+            data:imageList,
+            params:`${req.params.user} | ${req.params.composition}`
         })
     }catch (err){
         return next(new ErrorHandler(req,err,400))
